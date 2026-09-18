0806f1b949984c6c49890a5fefb74e055d947f0b
joshua-w-henry <83152332+joshua-w-henry@users.noreply.github.com>
2026-09-18 14:30:20 -0600

    Add production logger timing and storage configuration

--- include/config.h
+++ include/config.h
@@ -7,6 +7,12 @@ constexpr uint32_t CAN_BITRATE = 500000;
 
 constexpr uint32_t MQTT_FAST_PERIOD_MS = 1000;
 constexpr uint32_t MQTT_SLOW_PERIOD_MS = 10000;
-constexpr uint32_t WIFI_RETRY_PERIOD_MS = 10000;
+constexpr uint32_t MQTT_RETRY_PERIOD_MS = 5000;
+
+constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 8000;
+constexpr uint32_t WIFI_RETRY_PERIOD_MS = 2000;
+
+constexpr uint32_t SD_SPI_HZ = 4000000;
+constexpr uint32_t SD_FLUSH_PERIOD_MS = 1000;
 
 constexpr uint32_t RPM_CAN_ID = 0x0C665500UL;
