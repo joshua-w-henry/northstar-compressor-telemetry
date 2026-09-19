ed16539364636c6945d25acb38e119b0d90d4480
joshua-w-henry <83152332+joshua-w-henry@users.noreply.github.com>
2026-09-18 19:11:50 -0600

    Lower SD SPI speed and configure init retries

--- include/config.h
+++ include/config.h
@@ -12,7 +12,9 @@ constexpr uint32_t MQTT_RETRY_PERIOD_MS = 5000;
 constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 8000;
 constexpr uint32_t WIFI_RETRY_PERIOD_MS = 2000;
 
-constexpr uint32_t SD_SPI_HZ = 4000000;
+constexpr uint32_t SD_SPI_HZ = 1000000;
+constexpr uint8_t SD_INIT_ATTEMPTS = 3;
+constexpr uint32_t SD_INIT_RETRY_DELAY_MS = 300;
 constexpr uint32_t SD_FLUSH_PERIOD_MS = 1000;
 
 constexpr uint32_t RPM_CAN_ID = 0x0C665500UL;
